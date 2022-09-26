e4e11a8e2adab9d47252f7d36e84acf28b89243c
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-09-26 15:11:04 +0700

    SQL

---
+++ SQL/VIEW_INDEX_STORE/StoreProcedure.sql
@@ -0,0 +1,19 @@
+use classicmodels;
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+SELECT * FROM customers;
+END//
+DELIMITER ;
+
+call findAllCustomers();
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+SELECT * FROM customers WHERE customerNumber = 175;
+END //
+DELIMITER ;
+
+call findAllCustomers();
